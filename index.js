32ce223bf73023173f0b791be212b7ccc4214f7f
Neil Seejoor <nvseejoor@gmail.com>
2018-10-05 22:32:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function logDriverNames(arr) {
 function logDriversByHometown(arr, town) {
   arr.forEach(el => {
     if (el.hometown.localeCompare(town) === 0) {
-      console.log(el.hometown)
+      console.log(el.name)
     }
   })
 }
