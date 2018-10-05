8be4d497e6e18fd11486e778c4d8fdf11b140d3a
Neil Seejoor <nvseejoor@gmail.com>
2018-10-05 23:28:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-// Code your solution in this file!
-function logDriverNames(arr) {
-  arr.forEach(el => {
-    console.log(el.name)
-  })
-}
+const logDriverNames = function (drivers) {
+  drivers.forEach(function (driver) {
+    console.log(driver.name);
+  });
+};
 
-function logDriversByHometown(arr, town) {
-  arr.forEach(el => {
-    if (el.hometown.localeCompare(town) === 0) {
-      console.log(el.name)
+const logDriversByHometown = function (drivers, hometown) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
     }
-  })
-}
+  });
+};
 
-function driversByRevenue(arr) {
-  return arr.slice().sort(function (driverOne, driverTwo) {
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
     return driverOne.revenue - driverTwo.revenue;
-  })
-}
+  });
+};
 
 const driversByName = function (drivers) {
   return drivers.slice().sort(function (driverOne, driverTwo) {
-    return driverOne.name.localeCompare(driverTwo.name)
-  })
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
 
-  const totalRevenue = function (drivers) {
-    return drivers.reduce(function (total, currentDriver) {
-      return currentDriver.revenue + total;
-    }, 0);
-  };
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
 
-  const averageRevenue = function (drivers) {
-    return totalRevenue(drivers) / drivers.length;
-  };
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
